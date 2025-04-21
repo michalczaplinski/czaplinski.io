13138a38f4d620263ccc0c55a0e52d285cf8869e
Michal Czaplinski <mmczaplinski@gmail.com>
2025-04-21 10:59:03 +0100

    Update personal branding: correct spelling of name in various components, enhance global styles with a pastel color palette, and replace font with Lora for improved aesthetics. Remove About page for a more streamlined site structure.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Michal Czaplinski';
-export const SITE_DESCRIPTION = 'Software Engineer';
+export const SITE_TITLE = "Michał Czapliński";
+export const SITE_DESCRIPTION = "Software Engineer";
